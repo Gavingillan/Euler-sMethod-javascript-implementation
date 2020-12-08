4b120c100f53aa6deb0e79b5191e99f9d9be9208
Gavingillan <46040683+Gavingillan@users.noreply.github.com>
2020-12-07 23:15:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const { evaluate, expression, minTransformDependencies } = require('mathjs');
+const prompt = require('prompt');
+prompt.start();
+
+let scope = {
+
+}
+var derivative;
+var x;
+var y;
+var deltaX;
+var Iterations;
+startApp();
+
+function startApp(){
+    prompt.get(['derivative','InitialX','InitialY','DeltaX', 'Ending_X_Value'], function (err, result) {
+        if (err) { return onErr(err); }
+        derivative = result.derivative;
+        x=parseInt(result.InitialX);
+        y=parseInt(result.InitialY);
+        deltaX=parseFloat(result.DeltaX);
+        Iterations = parseInt(result.Ending_X_Value)/deltaX;
+        main();
+        startApp();
+    });
+}
+
+function main(){
+    scope.deltaX = deltaX;
+
+    console.log('first point: (' + x + ',' + y + ')');
+
+    for(var i=0;i<Iterations;i++){
+        scope.x= x;
+        scope.y=y;
+
+        scope.slope= evaluate(derivative,scope);
+
+        var newX= scope.x+scope.deltaX
+        scope.newX=newX;
+        var newY= evaluate(scope.slope*(newX-x)+y,scope)
+
+        x=newX;
+        y=newY;
+        console.log("point " + (i+2) + ": (" + x + ',' + y + ')' );
+    }
+}
+
